8b9fb4bbc8b563c9ec3cbc71f5fdb144ae0e800e
ILoveDjango3000 <ilovedjango3000@gmail.com>
2020-12-11 19:35:06 +0530

    Add static files configuration for local

--- core_ckeditor/settings.py
+++ core_ckeditor/settings.py
@@ -122,5 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR/"static_dir"]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_dir')]
 
 CKEDITOR_UPLOAD_PATH = "ckeditor"

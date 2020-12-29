f6deaa653530e47ba0db2d0a0fee2d38f49690db
ILoveDjango3000 <ilovedjango3000@gmail.com>
2020-12-29 22:52:15 +0530

    Add image compression on upload

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,15 @@
 import os
+import sys
 import uuid
+from io import BytesIO
 
 from ckeditor.fields import RichTextField
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
+from django.db import transaction
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from PIL import Image
 
 
 def get_file_path(instance, filename):
@@ -22,3 +29,35 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title[:20]
+
+
+@receiver(pre_save, sender=Article)
+@transaction.atomic
+def compress_uploaded_image(sender, instance, **kwargs):
+    print(instance)
+    try:
+        article = sender.objects.select_for_update().get(pk=instance.pk)
+    except sender.DoesNotExist:
+        # Its a new object
+        instance.image = compress_image(instance.image)
+    else:
+        if article.image != instance.image:
+            instance.image = compress_image(instance.image)
+
+
+def compress_image(uploaded_image):
+    image = Image.open(uploaded_image)
+    temp_image = image.convert("RGB")
+    temp_image = temp_image.resize((900, 570))
+    output_io_stream = BytesIO()
+    temp_image.save(output_io_stream, format="JPEG", quality=60)
+    output_io_stream.seek(0)
+    compressed = InMemoryUploadedFile(
+        output_io_stream,
+        "ImageField",
+        "%s.jpg" % uploaded_image.name.split(".")[0],
+        "image/jpeg",
+        sys.getsizeof(output_io_stream),
+        None,
+    )
+    return compressed
